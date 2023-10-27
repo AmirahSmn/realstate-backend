ab1f95e37e10348754762d6aa61f4fd483070ded
AmirahSmn <37636601+AmirahSmn@users.noreply.github.com>
2023-10-28 00:41:31 +0300

    Update index.js
    removed previous cors call

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./connect");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
 const port = 3000;
 const blogsRouter = require("./routes/blog");
@@ -12,7 +12,7 @@ const propertyRouter = require("./routes/property");
 
 require("dotenv").config();
 app.use(express.json({ limit: "500kb" }));
-app.use(cors());
+//app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
